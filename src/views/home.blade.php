e1087f69e9008a322a0f73fdf7b1162edf19d408
Łukasz Chachurski <lukasz.chachurski@baytekin.info>
2017-05-22 21:44:38 +0200

    improved authorization handling

--- src/views/home.blade.php
+++ src/views/home.blade.php
@@ -5,6 +5,33 @@
     <script src="http://cdn.auth0.com/js/lock/10.15.1/lock.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
     <script>
+
+        function authorizeUser(idToken) {
+            var http = new XMLHttpRequest();
+            var url = "/stauth/authorize";
+            var params = "token=" + idToken;
+
+            http.open("POST", url, true);
+            //Send the proper header information along with the request
+            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            http.setRequestHeader("Accept", "application/json");
+            http.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            http.setRequestHeader("X-CSRF-TOKEN", "{{ csrf_token() }}");
+
+            http.onreadystatechange = function () {
+
+                if (http.readyState === 4 && http.status === 200) {
+                    window.location = '{{ url('/') }}';
+                }
+
+                if (http.readyState === 4 && http.status === 403) {
+                    console.log('Unauthorized..');
+                }
+
+            };
+            http.send(params);
+        }
+
         function showLoginForm() {
             // Initiating our Auth0Lock
             var lock = new Auth0Lock(
@@ -32,40 +59,17 @@
 
             // Listening for the authenticated event
             lock.on("authenticated", function (authResult) {
-                // Use the token in authResult to getUserInfo() and save it to localStorage
                 lock.getUserInfo(authResult.accessToken, function (error, profile) {
                     if (error) {
                         // Handle error
                         return;
                     }
 
-                    localStorage.setItem('accessToken', authResult.accessToken);
-                    localStorage.setItem('profile', JSON.stringify(profile));
+                    var contentBox = document.getElementsByClassName('auth0-lock-confirmation-content')[0];
+                    contentBox.innerHTML += 'Waiting for authorization...';
 
-                    var http = new XMLHttpRequest();
-                    var url = "/stauth/authorize";
-                    var params = "token=" + authResult.idToken;
-
-                    http.open("POST", url, true);
-
-                    //Send the proper header information along with the request
-                    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                    http.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-                    http.setRequestHeader("X-CSRF-TOKEN", "{{ csrf_token() }}");
-
-                    http.onreadystatechange = function () {
-
-                        if (http.readyState === 4 && http.status === 200) {
-                            window.location = '{{ url('/') }}';
-                        }
-
-                        if (http.readyState === 4 && http.status === 403) {
-                            alert('unauthorized');
-                        }
-
-                    };
-                    http.send(params);
-
+                    authorizeUser(authResult.idToken);
+                    setInterval(authorizeUser, 10000, authResult.idToken);
                 });
             });
 
